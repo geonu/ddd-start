b49846eae29144496cedc18dfec92e91e44ff233
qodot <waitingforqodot@gmail.com>
2019-02-19 23:01:19 +0900

    인프라스트럭처 구현을 인터페이스로 분리하여 의존성 제거

--- service/order.py
+++ service/order.py
@@ -1,4 +1,8 @@
-from domain.order import Order
+from abc import ABC, abstractmethod
+from typing import List
+
+from domain.order import Order, OrderLine
+from domain.product import Money
 
 
 class OrderCancelService():
@@ -8,3 +12,29 @@ class OrderCancelService():
             raise ValueError(f'no order {order_id}')
 
         order.cancel()
+
+
+class RuleDiscounter(ABC):
+    @abstractmethod
+    def apply_rules(self, customer, order_lines: List[OrderLine]) -> Money:
+        pass
+
+
+class RuleDiscounterImpl(RuleDiscounter):
+    """ 실제로 룰 엔진을 구현하지는 않을 예정 """
+
+    def apply_rules(self, customer, order_lines: List[OrderLine]) -> Money:
+        pass
+
+
+class CalculateDiscountService():
+    _rule_discounter: RuleDiscounter
+
+    def __init__(self, rule_discounter: RuleDiscounter) -> None:
+        self._rule_discounter = rule_discounter
+
+    def calculate_discount(
+            self, order_lines: List[OrderLine], customer_id) -> Money:
+        customer = self.find_customer_by_id(customer_id)
+
+        return self._rule_discounter.apply_rules(customer, order_lines)
